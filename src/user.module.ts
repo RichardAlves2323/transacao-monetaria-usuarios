3fa91fe7114ac699afdbbc02e3bfd4912edd7fdb
Richard Alves <richardalves232323@gmail.com>
2025-07-09 21:24:05 -0300

    :sparkles: feat: Add User module and TypeORM configurations

---
+++ src/user.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserUseCase } from './domain/services/user.usecase';
+import { UserController } from './presentation/controllers/user.controller';
+import { UserTypeOrm } from './infrastructure/database/models/user.orm-entity';
+import { UserService } from './application/services/user.service';
+import { UserRepositoryByTypeOrm } from './infrastructure/database/repositories/user.repository.by-typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserTypeOrm])],
+  controllers: [UserController],
+  providers: [
+    UserService,
+    {
+      provide: 'UserRepository',
+      useClass: UserRepositoryByTypeOrm,
+    },
+    {
+      provide: UserUseCase,
+      useFactory: (userRepository: UserRepositoryByTypeOrm) => {
+        return new UserUseCase(userRepository);
+      },
+      inject: ['UserRepository'],
+    },
+  ],
+})
+export class UserModule {}
