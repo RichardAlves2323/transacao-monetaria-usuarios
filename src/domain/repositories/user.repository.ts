96acefdbf47aed499604a0889af825c10269bedb
Richard Alves <richardalves232323@gmail.com>
2025-07-09 21:14:28 -0300

    :sparkles: feat: Add user repository interface

---
+++ src/domain/repositories/user.repository.ts
@@ -0,0 +1,8 @@
+import { User } from '../entities/user.entity';
+
+export interface UserRepository {
+  save(user: User): Promise<User>;
+  findAll(): Promise<User[]>;
+  findById(id: string): Promise<User | null>;
+  findByUsername(username: string): Promise<User | null>;
+}
